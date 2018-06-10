532e01cbaac452901cf27cb8270a422b624beadb
Stražovan David <strazovan.d@gmail.com>
2018-06-10 23:29:58 +0200

    fixed disconnect message content type

--- js/ChatRoom.js
+++ js/ChatRoom.js
@@ -139,7 +139,7 @@ class ChatRoom {
     }
 
     _onClose(event) {
-        this._messageContainer.addMessage(new Message("Server", "message", "message/text", "You have been disconnected."))
+        this._messageContainer.addMessage(new Message("Server", "MESSAGE", "TEXT_MESSAGE", "You have been disconnected."))
         console.log(event)
     }
 
